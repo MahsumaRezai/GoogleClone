4a0a4676d357ccbef1a86dd9be201fb49206d355
unknown <mahsuma.rezai8@gmail.com>
2023-09-26 09:40:27 -0700

    add new image profile

--- src/Component/Head.js
+++ src/Component/Head.js
@@ -1,12 +1,29 @@
 import { Fragment } from "react";
 import React from 'react'
 import classes from "./Head.module.css";
+import img from './image/dit.PNG'
+
 const Head = (props) => {
 
     return (
         <Fragment>
             <div className={classes.head}>
-                <h2>hello mahsuam form the recat </h2>
+                <div>
+                    <div className={classes.postion}>
+                        <img src={img} alt="" className={classes.shap} />
+
+                    </div>
+                    <div>
+
+                    </div>
+                    <div>
+
+                    </div>
+
+
+
+
+                </div>
 
 
             </div>
