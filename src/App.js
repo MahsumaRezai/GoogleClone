15b9a221404f118087718163b3710a42f74166b3
unknown <mahsuma.rezai8@gmail.com>
2023-09-26 09:29:35 -0700

    work on head section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { Fragment } from "react";
 import React from 'react'
+import Head from "./Component/Head";
 
 function App(props) {
     return (
         <Fragment>
-            <h2>hello</h2>
+            <Head />
 
         </Fragment>
     )
--- src/Component/Head.js
+++ src/Component/Head.js
@@ -1,10 +1,13 @@
 import { Fragment } from "react";
 import React from 'react'
+import classes from "./Head.module.css";
 const Head = (props) => {
 
     return (
         <Fragment>
-            <div>
+            <div className={classes.head}>
+                <h2>hello mahsuam form the recat </h2>
+
 
             </div>
 
