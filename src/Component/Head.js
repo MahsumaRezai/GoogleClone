03ccf0c40900aed16275c1369e1ec236952d3f90
unknown <mahsuma.rezai8@gmail.com>
2023-09-26 10:06:31 -0700

    add head and work on it

--- src/Component/Head.js
+++ src/Component/Head.js
@@ -1,29 +1,31 @@
 import { Fragment } from "react";
 import React from 'react'
 import classes from "./Head.module.css";
-import img from './image/dit.PNG'
+import img from './image/dit.PNG';
+import img1 from './image/profile.jpg';
+import img2 from './image/chart.PNG';
+import img3 from './image/fersh.PNG';
+import img4 from './image/ondo.PNG';
+
 
 const Head = (props) => {
 
     return (
         <Fragment>
             <div className={classes.head}>
-                <div>
-                    <div className={classes.postion}>
-                        <img src={img} alt="" className={classes.shap} />
-
-                    </div>
-                    <div>
-
-                    </div>
-                    <div>
-
-                    </div>
+                <img src={img4} alt="" />
+                <img src={img3} alt="" />
+                <input className={classes.input} />
+                <img src={img2} alt="" />
+                <img src={img1} alt="" className={classes.profile} />
+                <img src={img} alt="" className={classes.shap} />
 
 
 
 
-                </div>
+
+
+
 
 
             </div>
